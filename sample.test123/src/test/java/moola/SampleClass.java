c106942ed8ecbd610312875e1918c1332b81e145
shashi448 <shashimoola448@gmail.com>
2017-06-28 11:24:33 -0500

    made changes

--- sample.test123/src/test/java/moola/SampleClass.java
+++ sample.test123/src/test/java/moola/SampleClass.java
@@ -5,6 +5,8 @@ public class SampleClass {
 	public static int a = 10, b = 20;
 
 	public static void main(String[] args) {
+		// adding changes to the same file
+		
 		System.out.println(a + b);
 	}
 
